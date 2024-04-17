5596da6c6ab5821f67096671916a9e67bbe37430
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-04-17 13:33:45 +0545

    added toogle menu in hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,60 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom
 
-const Navbar = () => {
-  return (
-  <>
 
-    <div className='NavbarDiv'>
+
+const Navbar = () => {
+  // const [isNavActive,setIsNavActive]=useState();
+  // const navRef = useRef();
+
+  // useEffect(() => {
+  //   if (navRef.current) {
+  //     if (isNavActive) {
+  //       navRef.current.classList.add("active");
+  //     } else {
+  //       navRef.current.classList.remove("active");
+  //     }
+  //   }
+  // }, [isNavActive]);
+  // const toggleNav = () => {
+  //   setIsNavActive((current) => !current);
+  // };
+
+  // const closeNav = () => {
+  //   setIsNavActive(false);
+  // };
+
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <>
+
+      <div className='hamburger'>
+        <RxHamburgerMenu onClick={toggleNavbar}  size={30} />
+      </div>
+      <div className={`NavbarDiv ${showNavbar ? 'show' : ''}`}>
+        <Link to="/" >Home</Link>
+        <Link to="/about">Categories</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/portfolio">cart</Link>
+        <Link to="/login">Login</Link>
+      </div>
+      {/* <div className='HamburgerMenu'>
         <Link to="/">Home</Link>
         <Link to="/about">Categories</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/portfolio">cart</Link>
-        <Link to="/login">Login</Link> 
-    </div>
-       <div className='hamburger'>
-       <RxHamburgerMenu size={40} />
-       </div>
-       </>
+        <Link to="/login">Login</Link>
+      </div> */}
+
+    </>
+
 
   )
 }
