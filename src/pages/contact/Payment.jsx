52b8e53735a0e7a72e7456f5c5466e646ccde052
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-04-19 15:09:52 +0545

    edited content of blog

--- src/pages/contact/Payment.jsx
+++ src/pages/contact/Payment.jsx
@@ -62,11 +62,11 @@ const Payment = () => {
       </div>
       <div class="card">
         <div class="card__header">
-          <img src="https://source.unsplash.com/600x400/?computer" alt="card__image" class="card__image" width="600" />
+          <img src="https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="card__image" class="card__image" width="600" />
         </div>
         <div class="card__body">
           <span class="tag tag-blue">Technology</span>
-          <h4>What's new in 2022 Tech</h4>
+          <h4>Learn React</h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!</p>
         </div>
         <div class="card__footer">
@@ -81,11 +81,11 @@ const Payment = () => {
       </div>
       <div class="card">
         <div class="card__header">
-          <img src="https://source.unsplash.com/600x400/?food" alt="card__image" class="card__image" width="600" />
+          <img src="https://plus.unsplash.com/premium_photo-1682145373962-4d76b19410ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D" alt="card__image" class="card__image" width="600" />
         </div>
         <div class="card__body">
-          <span class="tag tag-brown">Food</span>
-          <h4>Delicious Food</h4>
+          <span class="tag tag-brown">Automobiles</span>
+          <h4>Building Machines</h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!</p>
         </div>
         <div class="card__footer">
@@ -100,11 +100,11 @@ const Payment = () => {
       </div>
       <div class="card">
         <div class="card__header">
-          <img src="https://source.unsplash.com/600x400/?car,automobile" alt="card__image" class="card__image" width="600" />
+          <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D" alt="card__image" class="card__image" width="600" />
         </div>
         <div class="card__body">
-          <span class="tag tag-red">Automobile</span>
-          <h4>Race to your heart content</h4>
+          <span class="tag tag-red">Science</span>
+          <h4>Learn about Space and Gallexy</h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!</p>
         </div>
         <div class="card__footer">
